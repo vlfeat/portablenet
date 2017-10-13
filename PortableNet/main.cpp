98897d93bffac9363375ba1cae780dbc8e457088
Andrea Vedaldi <vedaldi@gmail.com>
2017-10-13 20:07:50 +0100

    Fix some data type and sign ambiguities.
    Adds basic support for Apple Accelerate BLAS.

--- PortableNet/main.cpp
+++ PortableNet/main.cpp
@@ -7,9 +7,10 @@
 //
 
 #include <iostream>
+#include "../Lib/matconvnet/matlab/src/bits/data.hpp"
 
 int main(int argc, const char * argv[]) {
-  // insert code here...
-  std::cout << "Hello, World!\n";
+
+
   return 0;
 }
