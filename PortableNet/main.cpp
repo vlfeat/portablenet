59517534398f47ab1e459bc1b7550e3da2fcd596
Andrea Vedaldi <vedaldi@gmail.com>
2017-10-13 16:03:44 +0100

    Initial Commit

---
+++ PortableNet/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PortableNet
+//
+//  Created by Andrea Vedaldi on 13/10/2017.
+//  Copyright © 2017 VGG. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
